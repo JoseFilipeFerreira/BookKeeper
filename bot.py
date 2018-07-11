1bcb49331b60ca5a001d01103b397a140bb6cc25
José FerreIra <jose.filipe.matos.ferreira@gmail.com>
2018-07-11 16:13:07 +0100

    working commit

--- bot.py
+++ bot.py
@@ -6,8 +6,9 @@ import os
 from os import path
 import subprocess
 import random
+import markovify
 
-bot = commands.Bot(command_prefix = '???')
+bot = commands.Bot(command_prefix = '-')
 
 bot.remove_command('help')
 
@@ -32,43 +33,20 @@ async def save(ctx, number):
     appInfo = await bot.application_info()
     if ctx.message.author == appInfo.owner:
         await bot.delete_message(ctx.message)
-        mgs = []
         number = int(number)
-        async for x in bot.logs_from(ctx.message.channel, limit = number):
-            mgs.append(x.content)
 
-        f = open("message.txt", "a")
-        for mg in mgs:
-            f.write(mg + "\n-\n")
-        f.close()
+        with open("message.txt", "a") as f:
+            async for x in bot.logs_from(ctx.message.channel, limit = number):
+                if x.content != "":
+                    msg = x.content
+                    msg.replace('\n', '.')
+                    f.write(x.content + '\n')  
 
 @bot.command(pass_context=True)
 async def generate(ctx):
-    f = open("message.txt", "r")
-    msg = f.read()
-    f.close()
-    msg = msg.split("\n-\n")
-    
-    for line in msg:
-        line = line.lower.split()
-        for i, word in enumerate(line):
-            if i == (len(line) - 1):
-                model['END'] = model.get('END', []) + [word]
-            else:
-                if i == 0:
-                    model['START'] = model.get('START', []) + [word]
-                model[word] = model.get(word, []) + [line[i+i]]
-
-    generated = []
-    while True:
-        if not generated:
-            words = model['START']
-        elif generated[-1] in model['END']:
-            break
-        else:
-            words = model = model[generated[-1]]
-        generated.append(random.choice(words))
-
-    print(generated)
+    with open("message.txt", "r") as f:
+        text = f.read()
+    text_model = markovify.NewlineText(text)
+    await bot.say(text_model.make_sentence(tries = 100))
 
 main()
