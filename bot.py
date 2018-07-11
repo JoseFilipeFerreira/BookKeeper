fe7ed018b75d4a522028bff7535922d8ab39456a
José FerreIra <jose.filipe.matos.ferreira@gmail.com>
2018-07-11 20:27:44 +0100

    messages done

--- bot.py
+++ bot.py
@@ -12,12 +12,18 @@ bot = commands.Bot(command_prefix = '-')
 
 bot.remove_command('help')
 
+text_model = None
+
 def main():
+    global text_model
+    with open("geral.txt", "r") as f:
+        text = f.read()
+        text_model = markovify.NewlineText(text)
     bot.run(open('auth').readline().rstrip())
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(game=discord.Game(name='snooping arround'))
+    await bot.change_presence(game=discord.Game(name='-generate'))
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -40,13 +46,38 @@ async def save(ctx, number):
                 if x.content != "":
                     msg = x.content
                     msg.replace('\n', '.')
-                    f.write(x.content + '\n')  
+                    msg.replace('@', '(a)')
+                    f.write(x.content + '\n')
+
+@bot.command(pass_context=True)
+async def saver(ctx, name):
+    appInfo = await bot.application_info()
+    if ctx.message.author == appInfo.owner:
+        await bot.change_presence(game=discord.Game(name='snooping arround'))
+        await bot.delete_message(ctx.message)
+        size = 100
+        lastMessage = None
+        with open("{}.txt".format(name), "a") as f:
+            while size == 100:
+                messages = bot.logs_from(ctx.message.channel,before = lastMessage, limit = 100)
+                size = 0             
+                async for x in messages:
+                    size += 1
+                    if x.content != "":
+                        msg = x.content
+                        msg.replace('\n', '.')
+                        msg.replace('@', '(a)')
+                        f.write(x.content + '\n')
+                    lastMessage = x
+                print(size)
+        await bot.change_presence(game=discord.Game(name='-generate'))
+                
 
 @bot.command(pass_context=True)
 async def generate(ctx):
-    with open("message.txt", "r") as f:
-        text = f.read()
-    text_model = markovify.NewlineText(text)
-    await bot.say(text_model.make_sentence(tries = 100))
+    global text_model
+    msg = text_model.make_sentence(tries = 100)
+    msg.replace('@', '(a)')
+    await bot.say(msg)
 
 main()
