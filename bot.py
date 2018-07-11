acb38b9477a9bad2d5892a160466e41454ead031
José FerreIra <jose.filipe.matos.ferreira@gmail.com>
2018-07-11 15:13:00 +0100

    first attempt

--- bot.py
+++ bot.py
@@ -2,63 +2,21 @@ import discord
 import asyncio
 import json
 from discord.ext import commands
-from datetime import datetime
 import os
 from os import path
 import subprocess
+import random
 
-bot = commands.Bot(command_prefix = '*')
+bot = commands.Bot(command_prefix = '???')
 
 bot.remove_command('help')
 
-#log variabels
-registo = '**Log pedidos JBB:**'
-nregisto = 0
-
-#extensions = ['games','quotes', 'programming','api', 'pokemon', 'ascii', 'youtube', 'menu', 'manage', 'memegenerator']
-extensions = ['api', 'manage']
-
-
-
-imagesMap = {}
-gifsMap = {}
-musicMap = {}
-
-voice_client = None
-player_client = None
-
-IMAGES_PATH = './Images/'
-GIFS_PATH = './Gif/'
-MUSIC_PATH = './Music/'
-
 def main():
-    for f in os.listdir(IMAGES_PATH):
-        if path.isfile(path.join(IMAGES_PATH, f)):
-            filename, file_ext = path.splitext(f)
-            imagesMap[filename.lower()] = f
-
-    for f in os.listdir(GIFS_PATH):
-        if path.isfile(path.join(GIFS_PATH, f)):
-            filename, file_ext = path.splitext(f)
-            gifsMap[filename.lower()] = f
-
-    for f in os.listdir(MUSIC_PATH):
-        if path.isfile(path.join(MUSIC_PATH, f)):
-            filename, file_ext = path.splitext(f)
-            musicMap[filename.lower()] = f
-
-    for extension in extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
-
     bot.run(open('auth').readline().rstrip())
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(game=discord.Game(name='*help'))
+    await bot.change_presence(game=discord.Game(name='snooping arround'))
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -67,74 +25,50 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-	await reactMessage(message)
+	await bot.process_commands(message)
 
-@bot.event
-async def on_message_edit(before, after):
-	await reactMessage(after)
-
-
-async def reactMessage(message):
-    if (message.content.lower() == 'push %ebp'):
-        await bot.send_message(message.channel, 'pop %recurso')
-    if message.content.startswith('*'):
-        content = message.content.lower()[1:]
-        if content in imagesMap:
-            await bot.send_file(
-                message.channel, IMAGES_PATH+imagesMap[content])
-            return
-        elif content in gifsMap:
-            await bot.send_file(
-                message.channel, GIFS_PATH+gifsMap[content])
-            return
-    await bot.process_commands(message)
-
-    global player_client
-    global voice_client
-    if player_client != None and player_client.is_playing() == False:
-        await voice_client.disconnect()
-        voice_client = None
-        player_client = None
-
-@bot.event
-async def on_member_join(member):
-    server = member.server
-    await bot.send_message(server.get_channel('418433020719136770'), 'Welcome to Selva MIEI! {0}'.format(member.mention))
-
-######################################################## MÚSICA
 @bot.command(pass_context=True)
-async def play(ctx, music):
-    if ctx.message.author.voice_channel:
-        music = music.lower()
-        if music in musicMap:
-            global voice_client
-            global player_client
-            if voice_client == None:
-               voice = await bot.join_voice_channel(ctx.message.author.voice_channel)
-               voice_client = voice
-            if player_client != None and player_client.is_playing():
-                await bot.say("Already Playing")
+async def save(ctx, number):
+    appInfo = await bot.application_info()
+    if ctx.message.author == appInfo.owner:
+        await bot.delete_message(ctx.message)
+        mgs = []
+        number = int(number)
+        async for x in bot.logs_from(ctx.message.channel, limit = number):
+            mgs.append(x.content)
+
+        f = open("message.txt", "a")
+        for mg in mgs:
+            f.write(mg + "\n-\n")
+        f.close()
+
+@bot.command(pass_context=True)
+async def generate(ctx):
+    f = open("message.txt", "r")
+    msg = f.read()
+    f.close()
+    msg = msg.split("\n-\n")
+    
+    for line in msg:
+        line = line.lower.split()
+        for i, word in enumerate(line):
+            if i == (len(line) - 1):
+                model['END'] = model.get('END', []) + [word]
             else:
-                player = voice_client.create_ffmpeg_player(MUSIC_PATH + music + ".mp3")
-                player_client = player
-                player.start()
-        else:
-        	await bot.say("Invalid Music")
-    else:
-    	await bot.say("You're not in a voice channel")
+                if i == 0:
+                    model['START'] = model.get('START', []) + [word]
+                model[word] = model.get(word, []) + [line[i+i]]
 
-@bot.command(pass_context=True)
-async def stop(ctx):
-    if ctx.message.author.voice_channel:
-        global voice_client
-        global player_client
-        if voice_client:
-            await voice_client.disconnect()
-            voice_client = None
-            player_client = None
+    generated = []
+    while True:
+        if not generated:
+            words = model['START']
+        elif generated[-1] in model['END']:
+            break
         else:
-        	await bot.say("JBB not in a voice channel")
-    else:
-    	await bot.say("You're not in a voice channel")
+            words = model = model[generated[-1]]
+        generated.append(random.choice(words))
+
+    print(generated)
 
 main()
